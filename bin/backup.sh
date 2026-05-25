8e46228faa2fbb1142e9d15088acf79aa8fa1df3
Thomas Baeckeroot <tbaeckeroot@sophiagenetics.com>
2026-05-25 09:14:32 +0200

    Include `nice` for backup to prevent "freeze" caused by overload

--- bin/backup.sh
+++ bin/backup.sh
@@ -7,4 +7,4 @@ DATE=$(date +"%Y-%m-%d")
 # Option --show-progress-size not available yet on mysqldump v10.19
 # Option --password="${DBPASSWORD}" not available on mysqldump v10.19
 nice -n 19 mysqldump --user="${DBUSER}" --password="${DBPASSWORD}" --databases "${DBNAME}" > "/volume1/homes/meteo/mysqldump_${DBNAME}_${DATE}.sql"
-gzip "/volume1/homes/meteo/mysqldump_${DBNAME}_${DATE}.sql"
+nice -n 19 gzip "/volume1/homes/meteo/mysqldump_${DBNAME}_${DATE}.sql"
